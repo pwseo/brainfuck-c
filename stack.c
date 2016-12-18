cea4dfa167d8a502530ae33e128ece6d729f36e1
Pedro Cunha <pwseo@posteo.org>
2016-12-18 17:52:43 +0000

    Fix indentation of stack code

--- stack.c
+++ stack.c
@@ -11,28 +11,34 @@ struct stack {
 };
 
 
-void *stack_pop(struct stack * const s) {
+void *stack_pop(struct stack * const s)
+{
     return s->items[--s->size];
 }
 
-void *stack_peek(struct stack const * const s) {
+void *stack_peek(struct stack const * const s)
+{
     return s->items[s->size - 1];
 }
 
-void stack_push(struct stack * const s, void *ptr) {
+void stack_push(struct stack * const s, void *ptr)
+{
     s->items[s->size++] = ptr;
 }
 
-bool stack_empty(struct stack const * const s) {
+bool stack_empty(struct stack const * const s)
+{
     return s->size == 0;
 }
 
-void stack_free(struct stack *s) {
+void stack_free(struct stack *s)
+{
     free(s->items);
     free(s);
 }
 
-struct stack *stack_new(void) {
+struct stack *stack_new(void)
+{
     struct stack *s = malloc(sizeof(*s));
 
     s->capacity = STACKSIZE;
