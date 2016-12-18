5ffeb64b496c00970102d91329d5e86c9ce09184
Pedro Cunha <pwseo@posteo.org>
2016-12-18 18:06:23 +0000

    Add proper driver code

--- bf.c
+++ bf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include "vm.h"
+
+#include "tokenizer_bf.h"
 
 int main(void) {
-    puts("Brainfuck.");
+    struct vm *vm = vm_new();
+
+    vm_load(vm, stdin, tokenizer_bf);
+    vm_execute(vm, stdin, stdout);
+    vm_free(vm);
 }
