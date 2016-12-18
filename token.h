f3ff66fd5b6ea321b126dec49b7cd0fdf0274740
Pedro Cunha <pwseo@posteo.org>
2016-12-18 18:02:42 +0000

    Add token datatype

---
+++ token.h
@@ -0,0 +1,13 @@
+#ifndef _pwseo_token_h
+#define _pwseo_token_h
+
+enum token {
+    TOK_INC, TOK_DEC,
+    TOK_LEFT, TOK_RIGHT,
+    TOK_LEFTB, TOK_RIGHTB,
+    TOK_GET, TOK_PUT,
+
+    TOK_HALT,
+};
+
+#endif
