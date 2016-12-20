7def70ff00fa66e97366bd01e54ab6d6a6a5bf39
Pedro Cunha <pwseo@posteo.org>
2016-12-20 16:12:21 +0000

    Make driver accept filename as argument
    This fixes #6 and makes it impossible (for now) to load scripts directly
    from stdin.

--- src/bf.c
+++ src/bf.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "vm.h"
 
 #include "tokenizer_bf.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s [FILE]\n", argv[0]);
+        exit(0);
+    }
+
+    FILE *bfsrc = fopen(argv[1], "r");
+
+    if (bfsrc == NULL) {
+        fprintf(stderr, "Could not open '%s' for reading.\n", argv[1]);
+        exit(1);
+    }
+
     struct vm *vm = vm_new();
 
-    vm_load(vm, stdin, tokenizer_bf);
+    vm_load(vm, bfsrc, tokenizer_bf);
     vm_execute(vm, stdin, stdout);
     vm_free(vm);
 }
